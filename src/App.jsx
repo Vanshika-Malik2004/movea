88f27e1990d968614db290298163afe8c3af0bee
Vanshika-Malik2004 <malikvanshika2004@gmail.com>
2023-06-08 02:03:48 +0530

    added gradient

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import LandingPage from "./pages/LandingPage";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
+    <>
       <LandingPage />
-    </div>
+    </>
   );
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./mainPage.css";
+import { BsArrowRight } from "react-icons/bs";
 const LandingPage = () => {
   return (
     <div className="main_container">
@@ -8,20 +9,21 @@ const LandingPage = () => {
         <div clasName="SignIn">Sign In</div>
       </nav>
       <div className="heading">
-        <div className="line line1">Watch Your</div>
-        <div className="line line2">Favourite Movies</div>
-        <div className="line line3">
-          With <span className="movea_style">MOVEA</span>
-        </div>
+        <h1>
+          Discover Movie Magic with Movea.Your Ultimate Entertainment
+          Destination
+        </h1>
       </div>
       <div className="content">
         Wwhether you're a fan ofheart-pounding action, gripping thrillers,
         heartwarming romances, mind-blowing sci-fi, or thought-provoking dramas,
         Movea has got you covered.
       </div>
-      <button>
-        <span className="btn">Get Started</span>
+      <button className="start_btn">
+        <span> Get Started</span>
+        <BsArrowRight className="right_arrow" />
       </button>
+      <div className="gradient_container"></div>
     </div>
   );
 };
