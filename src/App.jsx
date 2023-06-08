05c9494cac6df5435076330daea421ed48a2f385
tarunfy <tarunsharma8920@gmail.com>
2023-06-09 03:16:20 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import Genres from "./components/Genres";
 import movieList from "./components/movieList";
 function App() {
   return (
-    //<div className="app_main_container">
-    //  <Genres />
-    //  <movieList className="movie_list" />
-
-    //</div>
-    <LandingPage />
+    <div className="app_main_container">
+      <Genres />
+      <movieList className="movie_list" />
+    </div>
+    //<LandingPage />
   );
 }
 
